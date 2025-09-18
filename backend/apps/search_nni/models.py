ca90aa976ddf3681a29fd8e353dff12a89e47457
darwin <tranquynhanh691@gmail.com>
2025-09-19 01:24:22 +1000

    Defines data models for NNI search.
    Introduces Pydantic models for structuring NNI search requests
    and organisation details.
    
    Removes the old Django model.

--- backend/apps/search_nni/models.py
+++ backend/apps/search_nni/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from apps.common.enums.asic_enums import SearchType, SearchScope, EntityType, StatusType
+from apps.common.models import ASICBaseModel
+from typing import Optional, List, Annotated
+from pydantic import Field
 
-# Create your models here.
+class SearchNniOrganisation(ASICBaseModel):
+    """Organisation details for search NNI"""
+    name: Annotated[str, Field(min_length=1, max_length=200, description="The name of the organisation")]
+    type: Optional[Annotated[EntityType, Field(description="The type of the organisation")]] = None
+    status: Optional[Annotated[StatusType, Field(description="The status of the organisation")]] = None
+
+class SearchNniBody(ASICBaseModel):
+    """Request body for search NNI name"""
+    searchType: Annotated[SearchType, Field(description="The type of search to perform", default=SearchType.STANDARD)]
+    searchScope: Annotated[SearchScope, Field(description="The scope of the search", default=SearchScope.ALL_ENTITIES)]
+    organisation: Annotated[SearchNniOrganisation, Field(description="The organisation details to search for")]
+    maxResults: Optional[Annotated[int, Field(gt=0, le=100, description="Maximum number of results to return (1-100)")]] = 10
